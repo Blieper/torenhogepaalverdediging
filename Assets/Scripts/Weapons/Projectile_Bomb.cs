374c47c015f233583935ccf7c6f98d2731856314
Blieper <yoranmandema@eml.cc>
2018-04-30 15:57:12 +0200

    Explosive rounds

--- Assets/Scripts/Weapons/Projectile_Bomb.cs
+++ Assets/Scripts/Weapons/Projectile_Bomb.cs
@@ -3,9 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile_Bomb : Projectile {
+    public float ExplosionRadius = 5.0F;
+    public float ExplosionPower = 10.0F;
+    public float ExplosionTime = 2F;
 
-	// Use this for initialization
-	public override void Fire() {
+    // Use this for initialization
+    public override void Fire() {
         rb.velocity  = transform.TransformVector(new Vector3(0,0,100));
+
+        Invoke("Explode",ExplosionTime);
+    }
+
+    void Explode()
+    {
+        Vector3 explosionPos = transform.position;
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, ExplosionRadius);
+        foreach (Collider hit in colliders)
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+
+            print(rb);
+
+            if (rb != null)
+                rb.AddExplosionForce(ExplosionPower, explosionPos, ExplosionRadius, 3F);
+        }
+
+        Destroy(gameObject);
     }
 }
